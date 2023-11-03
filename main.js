172acd0abf50b82aac06980fde7a9eaa5ead3e4c
Sils <linardssils1@gmail.com>
2023-11-03 09:59:49 +0200

    Adding so that the style you pick goes to localstorage and saves it

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const list = document.querySelector(".colors");
+const documentElement = document.documentElement;
+const theme = localStorage.getItem("theme");
 
-onload = () => {
-  document.documentElement.classList.add("theme-grey");
-};
+documentElement.classList = "theme-grey";
+documentElement.classList.add(theme);
 
 list.childNodes.forEach((element) => {
   console.log(element.id);
   element.addEventListener("click", () => {
     document.documentElement.classList = "";
     document.documentElement.classList.add(`theme-${element.id}`);
+    localStorage.setItem("theme", `theme-${element.id}`);
   });
 });
