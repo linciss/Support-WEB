26e6e61d155e746b1d1e6c2161c3ea1ecfdf5840
Sils <linardssils1@gmail.com>
2023-11-10 10:05:32 +0200

    some changes to the slide

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const list = document.querySelector(".colors");
 const documentElement = document.documentElement;
 const theme = localStorage.getItem("theme");
+const next = document.querySelector(".next");
+const back = document.querySelector(".back");
 
 documentElement.classList = "theme-grey";
 documentElement.classList.add(theme);
@@ -9,26 +11,32 @@ list.childNodes.forEach((element) => {
   console.log(element.id);
   element.addEventListener("click", () => {
     document.documentElement.classList = "";
-    document.documentElement.classList.add(`theme-${element.id}`);
-    localStorage.setItem("theme", `theme-${element.id}`);
+    document.documentElement.classList.add(`theme-${element.id}-sils`);
+    localStorage.setItem("theme", `theme-${element.id}-sils`);
   });
 });
 
 let slideIndex = 0;
-
 const showSlides = (index) => {
   let slides = document.getElementsByClassName("slides");
   console.log(slides.length, index);
   if (index === slides.length) slideIndex = 0;
-  if (index < 0) slideIndex = slides.length;
+  if (index < 0) slideIndex = slides.length - 1;
+
   for (i = 0; i < slides.length; i++) slides[i].style.display = "none";
   slides[slideIndex].style.display = "block";
 };
 showSlides(slideIndex);
 
-const nextSlide = () => {
-  showSlides(slideIndex + 1);
-};
+next.addEventListener("click", () => {
+  slideIndex++;
+  showSlides(slideIndex);
+});
+
+back.addEventListener("click", () => {
+  slideIndex--;
+  showSlides(slideIndex);
+});
 
 setInterval(() => {
   slideIndex++;
